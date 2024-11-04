c48062124e703afebdf3116abaece65c7571085f
k3lly003 <ntirukelly@gmail.com>
2024-11-04 10:34:12 +0200

    initial

--- src/app/utils/fonts.js
+++ src/app/utils/fonts.js
@@ -1,7 +1,7 @@
 import { IBM_Plex_Sans } from 'next/font/google';
 
 const imb_plet = IBM_Plex_Sans({
-    subsets: [ 'regural' ],
+    subsets: [ 'latino' ],
     variable: '--font-ibm-plex-sans',
     weight: ['100', '200', '400', '600'],
     style: ['normal' ],
